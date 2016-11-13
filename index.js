ff2003213db070ebfcea25980728f6b867388519
abdullah ghani <abdullah.ghani3@gmail.com>
2016-11-14 02:13:39 +0530

    basic peer to peer

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-navigator.webkitGetUserMedia({video:true,audio:false},function(stream){
+
+var getUserMedia = require('getusermedia')
+
+getUserMedia({video:true,audio:true},function(err,stream){
+	
+
 	var Peer = require('simple-peer');
 	var peer = new Peer({
 		initiator: location.hash === '#init',
@@ -6,15 +11,20 @@ navigator.webkitGetUserMedia({video:true,audio:false},function(stream){
 		stream:stream
 	})
 
+	document.getElementById('connect').addEventListener('click',function(){
+		var otherId = JSON.parse(document.getElementById('otherId').value);
+		peer.signal(otherId);
+		var myvideo = document.createElement('video');
+		document.body.appendChild(myvideo);
+		myvideo.src = window.URL.createObjectURL(stream)
+	})
+
+
 	peer.on('signal',function(data){
 		document.getElementById('yourId').value = JSON.stringify(data);
 	})
 
-	document.getElementById('connect').addEventListener('click',function(){
-		var otherId = JSON.parse(document.getElementById('otherId').value);
-		peer.signal(otherId);
-	})
-
+	
 	document.getElementById('send').addEventListener('click',function(){
 		var message = document.getElementById('message').value;
 		document.getElementById('messages').textContent += 'You :'+ message + '\n'
@@ -28,10 +38,9 @@ navigator.webkitGetUserMedia({video:true,audio:false},function(stream){
 	peer.on('stream',function(streamData){
 		var video = document.createElement('video');
 		document.body.appendChild(video);
-		video.src = window.URL.createObjectURL(stream)
+		video.src = window.URL.createObjectURL(streamData)
 		video.play()
 	})
-},function(err){
-	console.log(err)
+
 })
 	
